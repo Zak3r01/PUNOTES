3485d51b09fb9d7abe09960f331c56a8c6c70899
Zakariye Maxamed <tisgot22zakmax@innovationsgymnasiet.se>
2024-05-27 11:05:38 +0200

    Got rid of extra code

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
-const notesSection = document.getElementById("notes");
+const likeButtons = document.querySelectorAll(".like-btn");
 
-const notes = [
-  {
-    title: "Tasty Chocalte Snack",
-    content:
-      "Adding milk chocolate, cinnamon, dipping buscuits, and freezing it is my go to snack!",
-    category: "Food",
-  },
-  {
-    title: "Favorite part in Dune 2",
-    content:
-      "When they are ambushing a spice harvester, and chani shoots down a hornithoptor!",
-    category: "Films",
-  },
-];
-
-function createNoteCard(note) {
-  const noteCard = document.createElement("div");
-  noteCard.classList.add("note-card");
-  noteCard.innerHTML = `
-    <h3>${note.title}</h3>
-    <p>${note.content}</p>
-    <p>Category: ${note.category}</p>
-  `;
-  return noteCard;
-}
-
-notes.forEach((note) => {
-  const noteCard = createNoteCard(note);
-  notesSection.appendChild(noteCard);
+likeButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    alert("Note Liked!");
+  });
 });
+function toggleMenu() {
+  var menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+}
